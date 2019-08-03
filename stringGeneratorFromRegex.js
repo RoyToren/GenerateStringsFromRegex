23d06c58f89664c994fb3c6079bfa9ad21db5890
roy <sajezz@gmail.com>
2019-08-03 12:32:47 +0300

    * added more functionality in user interface - need more work * refactored names and order of functions * added TODO'S

--- stringGeneratorFromRegex.js
+++ stringGeneratorFromRegex.js
@@ -2,15 +2,12 @@ const regexOperations = {
     ALTERNATION : '|',
     STAR : '*'
 };
-
-function isAction(currentChar) {
-    return (currentChar === regexOperations.ALTERNATION || currentChar === regexOperations.STAR);
-}
+const outputStringsList = document.getElementById('stringOutput');
+let genRe = undefined;
 
 function transformInputToPattern() {
-    let startPattern = [];
     let regexString = document.getElementById("regexInput").value.split('');
-    startPattern = regexString.map(el => {return isAction(el) ? [{action: el}] : [{val : el}]})
+    let startPattern = regexString.map(el => {return isAction(el) ? [{action: el}] : [{val : el}]})
         .reduce((acc, el) => {
             if(el[0].action === regexOperations.STAR){
                 acc[acc.length -1].action = regexOperations.STAR;
@@ -20,35 +17,12 @@ function transformInputToPattern() {
                 return  acc.concat(el);
             }
         });
-    // actionIndexes.reduce((_, el) => stack.concat(processPattern(currentPattern,el)));
-
-    take(100,runner(startPattern));
-}
-//const pattern =  [{val : '1', action: regexOperations.STAR},{val : '1'}, {action: regexOperations.ALTERNATION}, {val : '2'}, {action: regexOperations.ALTERNATION}, {val : '3'},{val : '4'},{val : '5'}];
-//let genRe = runner(pattern);
-
-function take(n, generator) {
-    let matches = '';
-    while (n>0){
-        let currentValue = generator.next().value;
-        if (currentValue !== undefined) {
-            matches += currentValue + ',';
-            console.log('left to calculate: ' + n);
-            n--;
-        } else {
-            console.log('There are no more string left to calculate');
-            break;
-        }
-    }
-    console.log(matches);
+    genRe = runner(startPattern);
+    invokeStringGeneration(100);
 }
 
-function transformPatternToStack(pattern) {
-        let stack = [[]];
-        pattern.forEach(function(element) {
-            element.action === regexOperations.ALTERNATION ? stack.push([]) : stack[stack.length - 1].push(element);
-        });
-        return stack;
+function isAction(currentChar) {
+    return (currentChar === regexOperations.ALTERNATION || currentChar === regexOperations.STAR);
 }
 
 function* runner(startPattern) {
@@ -65,31 +39,65 @@ function* runner(startPattern) {
     }
 }
 
+function invokeStringGeneration(amount) {
+    generateStringsByCurrentPattern(amount,genRe);
+}
+
+function generateStringsByCurrentPattern(n, generator) {
+    let matches = '';
+    while (n>0){
+        let currentValue = generator.next().value;
+        if (currentValue !== undefined) {
+            addToListOfStringsOutput(currentValue);
+            matches += currentValue + ',';
+            console.log('left to calculate: ' + n);
+            n--;
+        } else {
+            console.log('There are no more string left to calculate');
+            break;
+        }
+    }
+    console.log(matches);
+}
+
+function addToListOfStringsOutput(currentValue) {
+    let listViewItem = document.createElement('li');
+    listViewItem.appendChild(document.createTextNode(currentValue));
+    outputStringsList.appendChild(listViewItem);
+}
+
+function transformPatternToStack(pattern) {
+        let stack = [[]];
+        pattern.forEach(function(element) {
+            element.action === regexOperations.ALTERNATION ? stack.push([]) : stack[stack.length - 1].push(element);
+        });
+        return stack;
+}
+
 function processStack(stack) {
     const currentPattern = stack.shift();
-    return {value: stripRegex(currentPattern), stack: stack.length ? stack.concat(splitRegex(currentPattern)) : splitRegex(currentPattern)};
+    return {value: stripRegex(currentPattern), stack: stack.length ? stack.concat(splitRegexByStarAction(currentPattern)) : splitRegexByStarAction(currentPattern)};
 }
 
 function stripRegex(currentPattern) {
     return currentPattern.map(el => el.action === regexOperations.STAR ? el.val.substring(1) : el.val).join('');
-    //return currentPattern.map(el => el.val).join('');
 }
 
-function splitRegex(currentPattern) {
+function splitRegexByStarAction(currentPattern) {
     let actionIndexes = [];
     for(let i=0; i<currentPattern.length; i++){
         currentPattern[i].action === regexOperations.STAR ? actionIndexes.push(i) : undefined; //check if can use reducer to shorten the syntax
     }
     // const actionIndexes = currentPattern.filter(el => !!el.action).map((_, index) => index);
     let stack = [];
-    // actionIndexes.reduce((_, el) => stack.concat(processPattern(currentPattern,el)));
+    // actionIndexes.reduce((_, el) => stack.concat(processStarPattern(currentPattern,el)));
     actionIndexes.forEach(index => {
-        stack.push(processPattern(currentPattern,index));
+        stack.push(processStarPattern(currentPattern,index));
     });
     return stack;
 }
 
-function processPattern(currentPattern, actionIndex) {
+function processStarPattern(currentPattern, actionIndex) {
     let patternPrefix = currentPattern.slice(0,actionIndex);
     let currentElement = currentPattern[actionIndex];
     let currentElementChar = currentElement.val.substring(0,1);
@@ -101,20 +109,22 @@ function processPattern(currentPattern, actionIndex) {
 
 /*
 * TODO:
-*  DONE - need to handle * case. create array of pattern from regex - use library or try myself
 *  add empty string support
-*  add reset option + run again + if input is changed then reset as well
-*  refactor splitRegex + processPattern to support | and * together
-*  try to make splitRegex + processPattern more work more functional
-*  give meaningful names
 *  try to make no mutations and reduce created objects - only functions
-*  DONE -  fix | operator for multiple |
-*  DONE - make * work with case zero(shouldn't print them at first)
-*  DONE - add | operator support (process the regex before the generator and initialize the stack with right amount of sub arrays
-*  DONE - add "take" generator that give support of taking first n strings - from the internet
-*  DONE - give an interface to check the functionality
+*  refactor splitRegexByStarAction + processStarPattern to support | and * together ??
+*  try to make splitRegexByStarAction + processStarPattern more work more functional
+*  refactor code for review
+*  add comments to code
 *  give interesting test cases
 *  explain how it was built - flattened tree which is transformed into a stack (array of arrays of objects)
 *  go through the previous exercise (10 power of 10)
-*  add comments to code*/
+*  DONE - need to handle * case. create array of pattern from regex - use library or try myself
+*  DONE - give meaningful names
+*  DONE - add reset option + run again + if input is changed then reset as well
+*  DONE -  fix | operator for multiple |
+*  DONE - make * work with case zero(shouldn't print them at first)
+*  DONE - add | operator support (process the regex before the generator and initialize the stack with right amount of sub arrays
+*  DONE - add "generateStringsByCurrentPattern" generator that give support of taking first n strings - from the internet
+*  DONE - give an interface to check the functionality
+*/
 
