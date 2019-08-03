7fcc446bd604f1915186408cd5aa8a8866fd5cf0
roy <sajezz@gmail.com>
2019-08-03 19:15:55 +0300

    added empty char support

--- stringGeneratorFromRegex.js
+++ stringGeneratorFromRegex.js
@@ -1,6 +1,7 @@
 const regexOperations = {
     ALTERNATION : '|',
-    STAR : '*'
+    STAR : '*',
+    EMPTY : '<'
 };
 const outputStringsList = document.getElementById('stringOutput');
 let genRe = undefined;
@@ -31,7 +32,7 @@ function transformInputToPattern() {
 }
 
 function isAction(currentChar) {
-    return (currentChar === regexOperations.ALTERNATION || currentChar === regexOperations.STAR);
+    return (currentChar === regexOperations.ALTERNATION || currentChar === regexOperations.STAR || currentChar ===regexOperations.EMPTY);
 }
 
 function* runner(startPattern) {
@@ -54,20 +55,18 @@ function invokeStringGeneration() {
 }
 
 function generateStringsByCurrentPattern(n, generator) {
-    let matches = '';
     while (n>0){
         let currentValue = generator.next().value;
         if (currentValue !== undefined) {
             addToListOfStringsOutput(currentValue);
-            matches += currentValue + ',';
-            console.log('left to calculate: ' + n);
+            //console.log(currentValue);
+            //console.debug('left to calculate: ' + n);
             n--;
         } else {
             console.log('There are no more string left to calculate');
             break;
         }
     }
-    console.log(matches);
 }
 
 function addToListOfStringsOutput(currentValue) {
@@ -90,7 +89,17 @@ function processStack(stack) {
 }
 
 function stripRegex(currentPattern) {
-    return currentPattern.map(el => el.action === regexOperations.STAR ? el.val.substring(1) : el.val).join('');
+    return currentPattern.map(el => {
+        if(el.action === regexOperations.STAR){
+            return el.val.substring(1);
+        } else {
+            if(el.action === regexOperations.EMPTY)
+            {
+                return '';
+            } else {
+                return el.val;
+            }
+        }}).join('');
 }
 
 function splitRegexByStarAction(currentPattern) {
@@ -98,9 +107,7 @@ function splitRegexByStarAction(currentPattern) {
     for(let i=0; i<currentPattern.length; i++){
         currentPattern[i].action === regexOperations.STAR ? actionIndexes.push(i) : undefined; //check if can use reducer to shorten the syntax
     }
-    // const actionIndexes = currentPattern.filter(el => !!el.action).map((_, index) => index);
     let stack = [];
-    // actionIndexes.reduce((_, el) => stack.concat(processStarPattern(currentPattern,el)));
     actionIndexes.forEach(index => {
         stack.push(processStarPattern(currentPattern,index));
     });
@@ -127,6 +134,7 @@ function processStarPattern(currentPattern, actionIndex) {
 *  refactor code for review
 *  add comments to code
 *  give interesting test cases
+*  try and add webworkers for big input
 *  explain how it was built - flattened tree which is transformed into a stack (array of arrays of objects)
 *  go through the previous exercise (10 power of 10)
 *  DONE - need to handle * case. create array of pattern from regex - use library or try myself
